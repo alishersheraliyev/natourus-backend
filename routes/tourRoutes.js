e8f27ec70f83bd2a816f74fc1d5a26866c3d9359
Alisher Sheraliyev <alishersheraliyev18@gmail.com>
2026-01-09 09:46:32 +0500

    add alias

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,9 +7,12 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours
 } = require("./../controllers/tourController");
 
 // router.param("id", checkId);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
